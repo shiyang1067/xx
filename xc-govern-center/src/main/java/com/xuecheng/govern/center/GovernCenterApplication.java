dad005678d4a5c806035d6da011d81a451e91bc6
shiyang <370531324@qq.com>
2019-10-03 19:45:28 +0800

    测试持续集成

--- xc-govern-center/src/main/java/com/xuecheng/govern/center/GovernCenterApplication.java
+++ xc-govern-center/src/main/java/com/xuecheng/govern/center/GovernCenterApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * @author Administrator
+ * @author Administrato r
  * @version 1.0
  **/
 @EnableEurekaServer // 标识此工程是一个EurekaServer
